234b01087f5040bda48581a5ef4bfd71c2c543d7
Phil Remote <pschims@gmail.com>
2020-11-23 23:41:51 -0800

    Added bar chart for beer ABV

--- BeerPlot.py
+++ BeerPlot.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-excel_file_path = "AviationData.xlsx"
+excel_file_path = "Beers.xlsx"
 df = pd.read_excel(excel_file_path)  #"read_excel" is a method
 #print(df)
 
 #time series
+df_bar = df.groupby(['style']).mean()
+df_bar['abv'].plot.bar()
+plt.show()
